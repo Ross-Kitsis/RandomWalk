0783672524d9b9208188be7235177c74628b4aa6
Ross Kitsis <ross.kitsis@gmail.com>
2016-08-27 13:50:21 -0400

    Added getNumNode function

--- RandomWalk.c
+++ RandomWalk.c
@@ -12,8 +12,38 @@
 #include <stdlib.h>
 #include "RandomWalk.h"
 
-int main(void)
+
+int main(int agc, char *argv[])
 {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
+}
+
+
+/**
+ * Read adjacency file and return the number of nodes.
+ */
+int getNumNodes(char *fileName)
+{
+
+	FILE *file =fopen(fileName,"r");
+	int numNode = -1;
+
+	if (file == 0)
+	{
+		return -1;
+	}else
+	{
+		numNode = 0;
+		char *line = NULL;
+		int len = 0;
+		int read = 0;
+
+		while ((read=getline(&line,&len,&read))!= -1)
+		{
+			numNode++;
+		}
+	}
+
+	return numNode;
 }
