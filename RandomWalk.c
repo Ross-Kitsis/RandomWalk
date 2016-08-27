c9e7a7ea97d5654c59d674903a97ae12e59456f4
Ross Kitsis <ross.kitsis@gmail.com>
2016-08-27 10:35:05 -0400

    Initial commit with node struct

---
+++ RandomWalk.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : RandomWalk.c
+ Author      : 
+ Version     :
+ Copyright   : 
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "RandomWalk.h"
+
+int main(void)
+{
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ RandomWalk.h
@@ -0,0 +1,20 @@
+/*
+ * RandomWalk.h
+ *
+ *  Created on: 2016-08-27
+ *      Author: ross
+ */
+
+#ifndef RANDOMWALK_H_
+#define RANDOMWALK_H_
+
+
+
+struct Node
+{
+	int Id;
+	struct Node *edges;
+};
+
+
+#endif /* RANDOMWALK_H_ */
